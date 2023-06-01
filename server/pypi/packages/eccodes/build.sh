616d84b139c0859892026fe624cf1f013cbadbc8
TREMARE <81695088+ytremare@users.noreply.github.com>
2023-06-01 10:43:54 +0200

    Update build.sh
    Add DIEEE_BE &DIEEE_LE options

--- server/pypi/packages/eccodes/build.sh
+++ server/pypi/packages/eccodes/build.sh
@@ -3,33 +3,28 @@ set -eu
 ls -lrt
 echo "====================================================================="
 echo "====================================================================="
-echo "=============§§§§§§§§§§§ $PREFIX §§§§§§§§§§§§§§§§§§§§§§============="
+echo "============ $PREFIX ============="
 echo "====================================================================="
 pwd
-ECCODES_SRCDIR=$(pwd)
-echo $ECCODES_SRCDIR
+#ECCODES_SRCDIR=$(pwd)
+#echo $ECCODES_SRCDIR
+mkdir -p build
+cd build
 rm -f CMakeCache.txt  # For rerunning with build-wheel.py --no-unpack.
-cd ../../
-echo "=================?????????????????????????????======================="
-echo "====================================================================="
-
 ls -lrt 
-#mkdir -p ecbuild
-#cd ecbuild
-pwd                  
+pwd                
 echo "======================================================================================================="
 echo "=================================           Start cmake                        ========================"
-echo "=======================================================================================================" 
+echo "======-DIEEE_LE=1 =======================================================================================" 
 #cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  
-cmake /$ECCODES_SRCDIR -C /home/ytrem/miniconda3/chaquopy-tremare_eccodes/server/pypi/packages/eccodes/TryRunResults.cmake -DDISABLE_OS_CHECK=ON -DENABLE_AEC=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DENABLE_FORTRAN=OFF -DCOMPILE_TESTS=NO -DCMAKE_INSTALL_PREFIX=install-eccodes 
+cmake .. -DIEEE_LE=1 -DIEEE_BE=1 \
+-DDISABLE_OS_CHECK=ON -DENABLE_AEC=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DENABLE_FORTRAN=OFF -DCOMPILE_TESTS=NO -DBUILD_SHARED_LIBS=1 \
+   -DCMAKE_INSTALL_PREFIX="$PREFIX" 
 echo "======================================================================================================="
 echo "======================================================================================================="
 echo "=====================      cmake ends      ignore  ctest    ==========================================="
 echo "======================================================================================================="
 echo "======================================================================================================="
-#ctest
-#make | grep  'grib_accessor_class_data_sh_packed'
-
-make -j $(nproc) 
-echo "=====================      Fin Installation ECCODES      ================================"
+make  -j $(nproc) 
+echo "=====================      Fin make Process ECCODES      ================================"
 make install
